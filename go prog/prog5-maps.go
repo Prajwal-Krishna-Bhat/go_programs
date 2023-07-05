ca9110720e72ae28045d64a084ced6b36b3cb96d
Prajwal-Krishna-Bhat <133648728+Prajwal-Krishna-Bhat@users.noreply.github.com>
2023-07-05 09:12:27 +0000

    done

--- go prog/prog5-maps.go
+++ go prog/prog5-maps.go
@@ -1,31 +1,33 @@
 package main
+
 import "fmt"
-func main(){
-	myMap:=make(map[string]int)
-	myMap["apple"]=1;
-	myMap["banana"]=2
-	myMap["orange"]=3
 
-	appleValue:=myMap["apple"]
-	bananaValue:=myMap["banana"]
+func main() {
+	myMap := make(map[string]int)
+	myMap["apple"] = 1
+	myMap["banana"] = 2
+	myMap["orange"] = 3
 
-	fmt.Println("Value of apple:",appleValue)
-	fmt.Println("Value of banana:",bananaValue)
+	appleValue := myMap["apple"]
+	bananaValue := myMap["banana"]
 
-	myMap["apple"]=5
-	fmt.Println("Updated value of apple",myMap["apple"])
+	fmt.Println("Value of apple:", appleValue)
+	fmt.Println("Value of banana:", bananaValue)
 
-	delete(myMap,"orange")
-	fmt.Println("After deleting orange:",myMap)
-	
-	value, exists:=myMap["banana"]
-	if exists{
-		fmt.Println("Value of banana:",value)
-	}else{
+	myMap["apple"] = 5
+	fmt.Println("Updated value of apple", myMap["apple"])
+
+	delete(myMap, "orange")
+	fmt.Println("After deleting orange:", myMap)
+
+	value, exists := myMap["banana"]
+	if exists {
+		fmt.Println("Value of banana:", value)
+	} else {
 		fmt.Println("Banana not found in the map")
 	}
-	for key, value:=range myMap{
-		fmt.Println("Key:",key,"Value:",value)
+	for key, value := range myMap {
+		fmt.Println("Key:", key, "Value:", value)
 	}
-	fmt.Println("Length of the map:",len(myMap))
+	fmt.Println("Length of the map:", len(myMap))
 }
